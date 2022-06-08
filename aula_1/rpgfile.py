4a097f8d82f1188957e1b882ee65b81d7b727727
Daniel Ferreira <66141645+Danib0y97@users.noreply.github.com>
2022-06-08 17:31:01 -0300

    RPG File

---
+++ aula_1/rpgfile.py
@@ -0,0 +1,9 @@
+name = "octavio"  #string
+age = 50 #int
+height = 1.80 #float
+weight = 70 #int
+peacefull =  True or False #Boolean
+hair = "red" #string
+eyes = "yellow" #string
+
+print("octavio")
