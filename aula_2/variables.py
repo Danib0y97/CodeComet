6f1a7fb5b286df6c8a502a807d70b232496ba6e5
Daniel Ferreira <66141645+Danib0y97@users.noreply.github.com>
2022-06-22 18:05:33 -0300

    demonstração

---
+++ aula_2/variables.py
@@ -0,0 +1,24 @@
+a = 1 
+b = 5
+c = "ball"
+d = 10.4 
+e = 9**2
+f = 5 + 5 
+g = "coffee"
+h = 5*25
+i = 6
+j = 3 
+
+
+print(a + d)
+print(a - f)
+print(a * f)
+print(f / b) #-> result of division 
+print(f ** b)
+print (i % j)# - >rest of division 
+print (c + g)
+print (f % j)
+print (f // j)# -> "arredondado"
+
+def sum(a , b):
+  return a + b
